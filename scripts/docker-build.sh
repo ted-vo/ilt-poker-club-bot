e791d1d0d0f8d5dee956c56619e119c851b769cf
tedvo <vtnhon.dk@gmail.com>
2022-11-21 23:40:14 +0700

    release: v1.0.4

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 HOST_NAME="asia.gcr.io"
 PROJECT_ID="meepo-vn"
 IMAGE_NAME="ilt-poker-club-bot"
-IMAGE_VERSION="v1.0.3"
+IMAGE_VERSION="v1.0.4"
 
 TAG="$HOST_NAME/$PROJECT_ID/$IMAGE_NAME:$IMAGE_VERSION"
 
